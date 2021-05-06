449eee340f71a0ba891a84861574f9bda32c8188
Mateo Usuga <mursugaperez@gmail,com>
2021-05-05 20:53:25 -0500

    funcionalidad lista

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var posiciones = [0,-160,-320,-481,-642,-803];
 var dado1,dado2,boton_tirar;
 var total_tiro;
 var turno = 1;
+var punto;
 
 window.onload = init;
 
@@ -37,20 +38,33 @@ function jugar(){
 	alert("ganaste la partida");
 	turno = 1;
 	console.log("se reinició el turno")
+	window.location.reload();
 	}
 if((turno==1) && (total_tiro==2 || total_tiro==3 || total_tiro==12))
     {
         alert("perdiste la partida");
         turno = 1;
         console.log("se reinició el turno")
+        window.location.reload();
     }
       if((turno>=2) && (total_tiro==7))
     {
         alert("perdiste la partida");
         turno = 1;
         console.log("se reinició el turno")
-        
-    
+        window.location.reload();
     }
+    if((turno>=2) && (total_tiro==punto))
+    {
+        alert("ganaste la partida");
+        turno = 1;
+        console.log("se reinició el turno")
+        window.location.reload();
+    }
+    if((turno==1) && (total_tiro==4 || total_tiro==5 || total_tiro==6 || total_tiro==8 || total_tiro==9 || total_tiro==10))
+    {
+        punto=total_tiro
+    }
+
 turno = turno + 1;	
 }
